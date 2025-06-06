7989bb028a4cd915f23b524bc944a5d29d23646c
John Heikens <real.johnheikens@gmail.com>
2025-06-06 18:42:13 +0200

    finished basic setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,33 @@
-int main() {
-	return 0;
+#include "application/initialize.h"
+#include "application/control/form/form.h"
+#include "application/application.h"
+#include "gameInfo.h"
+
+struct gameForm : public form
+{
+	gameForm()
+	{
+	}
+	void updateStream()
+	{
+	}
+	virtual void keyDown(cvk& keyCode) override
+	{
+	}
+	virtual void mouseDown(cveci2& position, cmb& button) override
+	{
+	}
+	virtual void scroll(cveci2& position, cint& scrollDelta) override
+	{
+	}
+	virtual void render(cveci2& position, const texture& renderTarget) override
+	{
+	}
+};
+gameForm* mainForm = new gameForm();
+int main(int argc, char* argv[])
+{
+	// execute this function before you do anything,
+	initialize();
+	return application(mainForm, gameName).run();
 }
